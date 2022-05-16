1bde363b06e5e629bfa9af2974881531fa127852
austin <scrive.austi@awatapu.school.nz>
2022-05-16 13:59:14 +1200

    things

--- ask_snacks/snacks_to_total.py
+++ ask_snacks/snacks_to_total.py
@@ -1,4 +1,12 @@
+snacks = ['popcorn']
+
+
 
 def snacks_total(snacks):
-    length_of_snacks = len(snacks) -1
+    length_of_snacks = len(snacks)
     for i in range(0,length_of_snacks):
+
+
+
+if __name__ == '__main__':
+    snacks_total(snacks)
